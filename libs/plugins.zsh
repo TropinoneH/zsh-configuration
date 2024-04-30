1c0ed4f62701a351015eab736568254324ca6d85
TropinoneH <mspt5871@protonmail.com>
2024-04-30 15:04:01 +0800

    add plugin: zsh-z

--- libs/plugins.zsh
+++ libs/plugins.zsh
@@ -1,9 +1,14 @@
 # plugins and configurations
 export PLUGINS=$HOME/.zsh/plugins
+
 source $PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 source $PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh
+
 source $PLUGINS/history/history.plugin.zsh
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=8"
+
 source $PLUGINS/git/git.plugin.zsh
-source $PLUGINS/autojump/autojump.plugin.zsh
+
+source $PLUGINS/zsh-z/zsh-z.plugin.zsh
