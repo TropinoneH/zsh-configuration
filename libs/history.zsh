93a219240007486d60c3bfbe5eede75823da699c
TropinoneH <mspt5871@protonmail.com>
2024-05-06 16:48:50 +0800

    change histroy save limits

--- libs/history.zsh
+++ libs/history.zsh
@@ -1,8 +1,12 @@
 # use history in .zsh_history
-HISTFILE=$HOME/.zsh_history
-HISTSIZE=100000
-SAVEHIST=100000
+
+## History file configuration
+[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
+[ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
+[ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
+
 setopt appendhistory
+setopt extended_history
 setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
